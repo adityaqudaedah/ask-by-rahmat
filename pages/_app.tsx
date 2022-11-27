5b7b8b3055a9c32410581002147580b58b954f9b
adityaqudaedah <adityaqudaedah@gmail.com>
2022-11-27 16:21:40 +0700

    Add Feature

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,31 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import '../styles/globals.css';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import {
+  Session,
+  SessionContextProvider,
+  useSession,
+} from '@supabase/auth-helpers-react';
+import { useState } from 'react';
+
+interface ISupabaseSession {
+  initialSession: Session;
+}
+
+export default function App({
+  Component,
+  pageProps,
+}: AppProps<ISupabaseSession>) {
+  const [supabase] = useState(() => createBrowserSupabaseClient());
+  return (
+    <SessionContextProvider
+      supabaseClient={supabase}
+      initialSession={pageProps.initialSession}
+    >
+      <main className='min-h-screen min-w-full'>
+        <Component {...pageProps} />
+      </main>
+    </SessionContextProvider>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,30 @@
-import Head from 'next/head'
-import Image from 'next/image'
+//supabase import
+import { Auth, ThemeSupa } from '@supabase/auth-ui-react';
+import { useSupabaseClient, useSession } from '@supabase/auth-helpers-react';
+import HomePage from '../components/account';
+import Layout from '../components/layout';
+import Card from '../components/card';
 
-export default function Home() {
+const IndexPage = () => {
+  const session = useSession();
+  const supabase = useSupabaseClient();
+
   return (
-    <h1 className="text-3xl font-bold">
-      Hello world!
-    </h1>
-  )
-}
+    <>
+      {!session ? (
+        <div className='flex flex-col items-center justify-center space-y-10 max-w-screen-md min-h-screen mx-auto'>
+          <h1 className='text-2xl font-bold text-[#8EC3B0]'>Ask By Rahmat</h1>
+          <Card>
+            <Auth appearance={{ theme: ThemeSupa }} supabaseClient={supabase} />
+          </Card>
+        </div>
+      ) : (
+        <Layout>
+          <HomePage session={session} />
+        </Layout>
+      )}
+    </>
+  );
+};
 
+export default IndexPage;
