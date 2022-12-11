6e33fbfde7599b47ce160cbff988de71d63d4adb
adityaqudaedah <adityaqudaedah@gmail.com>
2022-12-11 11:41:41 +0700

    refactoring types

---
+++ types/schema.d.ts
@@ -0,0 +1,70 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json }
+  | Json[]
+
+export interface Database {
+  public: {
+    Tables: {
+      posts: {
+        Row: {
+          id: number
+          created_at: string | null
+          post_owner_id: string | null
+          message: string | null
+        }
+        Insert: {
+          id?: number
+          created_at?: string | null
+          post_owner_id?: string | null
+          message?: string | null
+        }
+        Update: {
+          id?: number
+          created_at?: string | null
+          post_owner_id?: string | null
+          message?: string | null
+        }
+      }
+      profiles: {
+        Row: {
+          id: string
+          updated_at: string | null
+          username: string | null
+          full_name: string | null
+          avatar_url: string | null
+        }
+        Insert: {
+          id: string
+          updated_at?: string | null
+          username?: string | null
+          full_name?: string | null
+          avatar_url?: string | null
+        }
+        Update: {
+          id?: string
+          updated_at?: string | null
+          username?: string | null
+          full_name?: string | null
+          avatar_url?: string | null
+        }
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      install_available_extensions_and_test: {
+        Args: Record<PropertyKey, never>
+        Returns: boolean
+      }
+    }
+    Enums: {
+      [_ in never]: never
+    }
+  }
+}
+
