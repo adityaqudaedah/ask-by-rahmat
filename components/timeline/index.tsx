5ef526bb3e87c90d751615f60aedc8618ee0a25c
adityaqudaedah <adityaqudaedah@gmail.com>
2022-12-18 01:07:04 +0700

    refactoring

--- components/timeline/index.tsx
+++ components/timeline/index.tsx
@@ -1,4 +1,3 @@
-import { useUser } from '@supabase/auth-helpers-react';
 import Link from 'next/link';
 import React from 'react';
 import { Database } from 'types';
@@ -7,12 +6,9 @@ import TimelineList from './TimelineList';
 const TimeLine: React.FC<{
   posts: Array<Database['public']['Tables']['posts']['Row']>;
 }> = ({ posts }) => {
-  const user = useUser();
   return (
     <div className='flex flex-col w-full h-screen space-y-4 mt-10'>
-      <h1 className='font-semibold text-lg text-slate-500'>
-        {user ? 'My Timeline' : 'Timeline'}
-      </h1>
+      <h1 className='font-semibold text-lg text-slate-500'>Timeline</h1>
       {/* timeline list item */}
       {posts?.map((post) => (
         <TimelineList key={post.id} message={post.message!} />
